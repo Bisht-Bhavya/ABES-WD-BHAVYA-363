d03ee1404f4b629da9714b78e83ba13bce60f159
Bisht-Bhavya <uniwrk33@gmail.com>
2025-10-15 12:59:56 +0530

    completed skeleton experiment 5

---
+++ Experiment_1/cartincrement.js
@@ -0,0 +1,4 @@
+    let valueElement = document.getElementById("value");
+    let currentValue = parseInt(valueElement.innerText);
+        currentValue++; // Increments by 1
+        valueElement.innerText = currentValue;
